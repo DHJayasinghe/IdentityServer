991ab5b602ea27e7bee0666c99a0d265810e7163
Dhanuka Jayasinghe <hasitha2kandy@gmail.com>
2021-01-24 15:12:43 +0530

    Added docker support

--- Identity.API/Program.cs
+++ Identity.API/Program.cs
@@ -1,5 +1,8 @@
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
+using Serilog;
+using System;
 
 namespace Identity.API
 {
@@ -7,6 +10,32 @@ namespace Identity.API
     {
         public static void Main(string[] args)
         {
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .AddEnvironmentVariables()
+                .Build();
+
+            string application = configuration["ApplicationName"];
+
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(configuration)
+                .WriteTo.Console()
+                .CreateLogger();
+
+            try
+            {
+                Log.Information($"{application} application starting up");
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, $"{application} application failed to start");
+            }
+            finally
+            {
+                // close and dispose the logging system correctly
+                Log.CloseAndFlush();
+            }
             CreateHostBuilder(args).Build().Run();
         }
 
